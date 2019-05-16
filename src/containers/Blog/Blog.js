8ffd054a874ac14791b5d34d625b59679103f7dd
stefian <v_ianta@yahoo.com>
2019-05-16 16:19:20 -0400

    added redirect Guards in Blog and NewPost comps

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,6 +8,10 @@ import NewPost from './NewPost/NewPost';
 // import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
+    state = {
+        auth: false
+    }
+
     render() {
         return (
             <div className="Blog">
@@ -34,7 +38,7 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1> }/> */}
                 
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                     {/* <Route path="/" component={Posts} /> */}
