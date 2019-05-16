648e567744ccf60c5dc8494914fdac9e89b15f6c
stefian <v_ianta@yahoo.com>
2019-05-16 11:54:07 -0400

    Navigating programmatically with history.push(pathname + id)

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import classes from './Posts.module.css';
@@ -32,7 +32,8 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id });
+        // this.props.history.push({ pathname: '/' + id });
+        this.props.history.push('/' + id);
     };
 
     render() {
@@ -40,16 +41,18 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link to={'/' + post.id} key={post.id}>
-                        <Post
-                            title={post.title}
-                            author={post.author}
-                            // sending all parent props with a spread operator
-                            // {...this.props}
-                            // or sending just one prop
-                            // match={this.props.match}
-                            clicked={() => this.postSelectedHandler(post.id)} />
-                    </Link>);
+                    // <Link to={'/' + post.id} key={post.id}>
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        // sending all parent props with a spread operator
+                        // {...this.props}
+                        // or sending just one prop
+                        // match={this.props.match}
+                        clicked={() => this.postSelectedHandler(post.id)} />
+                    // </Link>
+                );
             });
         }
 
