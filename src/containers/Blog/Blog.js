526137452d56c41510be31c4e3668aa5daad8ea1
stefian <v_ianta@yahoo.com>
2019-05-16 16:44:01 -0400

    handling the 404 Case / Unknown routes in Landing Blog comp

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -40,7 +40,9 @@ class Blog extends Component {
                 <Switch>
                     {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
                     <Route path="/posts" component={Posts} />
-                    <Redirect from="/" to="/posts" />
+                    <Route render={() => <h1>Not Found</h1>}/> 
+                    {/* above and below - catch all routes - to be last in the list */}
+                    {/* <Redirect from="/" to="/posts" /> */}
                     {/* <Route path="/" component={Posts} /> */}
                 </Switch>
             </div>
