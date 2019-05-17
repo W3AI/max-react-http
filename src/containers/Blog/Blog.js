b03dde1e164b911aa05cdecd1a4cbece6ff2c7b8
stefian <v_ianta@yahoo.com>
2019-05-16 20:24:41 -0400

    added hoc asyncComp for Loading routes Lazily for NewPost comp

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-// import FullPost from './FullPost/FullPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
 
     render() {
@@ -38,7 +42,7 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1> }/> */}
                 
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Route render={() => <h1>Not Found</h1>}/> 
                     {/* above and below - catch all routes - to be last in the list */}
